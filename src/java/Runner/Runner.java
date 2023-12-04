06508d276bb5bf93648494ab2a51e1703c2c767c
Diego Yanez Gonzalez <diego.yanez@tsoftlatam.com>
2023-12-04 19:37:40 -0300

    Se agrega Runner

--- src/java/Runner/Runner.java
+++ src/java/Runner/Runner.java
@@ -1,6 +1,15 @@
 package Runner;
 
+import org.junit.runner.RunWith;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
 @RunWith(Cucumber.class)
+@CucumberOptions(
+    features = "src/java/Steps/APISteps.java",
+    glue = "Steps"
+)
 
 public class Runner {
     
